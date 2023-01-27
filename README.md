8d0593bc408575f6ed36ccc60c373e6a250456d1
xNovo3000 <fontana.ciro.2001@gmail.com>
2023-01-27 11:23:18 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# EVENTUS
+
+Event management system (BE/FE)
---
+++ src/test/java/io/github/xnovo3000/eventus/EventusApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.xnovo3000.eventus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
