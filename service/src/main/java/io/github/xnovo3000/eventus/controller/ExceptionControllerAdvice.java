67b657521b7f5b1739585c8f7701d9ca50fd86c3
xNovo3000 <fontana.ciro.2001@gmail.com>
2023-04-21 12:30:22 +0200

    Exception logged in the log file, HTML button fix

--- service/src/main/java/io/github/xnovo3000/eventus/controller/ExceptionControllerAdvice.java
+++ service/src/main/java/io/github/xnovo3000/eventus/controller/ExceptionControllerAdvice.java
@@ -1,5 +1,6 @@
 package io.github.xnovo3000.eventus.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -8,11 +9,15 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
+@Slf4j
 public class ExceptionControllerAdvice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public String handleValidationError(Model model) {
+    public String handleValidationError(Model model, MethodArgumentNotValidException exception) {
+        // Log exception anyway
+        log.error("Application error", exception);
+        // Set model for exception HTML and render it
         model.addAttribute("header", "400 - Bad Request");
         model.addAttribute("content", "La richiesta inviata non risulta valida. Se si ritiene sia un errore, contattare l'amministratore");
         return "page/exception";
@@ -20,7 +25,10 @@ public class ExceptionControllerAdvice {
 
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public String handleInternalServerError(Model model) {
+    public String handleInternalServerError(Model model, Exception exception) {
+        // Log exception anyway
+        log.error("Application error", exception);
+        // Set model for exception HTML and render it
         model.addAttribute("header", "500 - Internal Server Error");
         model.addAttribute("content", "Errore dell'applicazione, contattare l'amministratore");
         return "page/exception";
