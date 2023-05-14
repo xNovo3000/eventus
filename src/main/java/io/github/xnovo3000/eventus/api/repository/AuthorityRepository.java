70d5d5b489aeb851e638f0ea6364eff5dd83feb8
xNovo3000 <fontana.ciro.2001@gmail.com>
2023-05-14 16:08:54 +0200

    Removed unused code, updated CHANGELOG.md

--- src/main/java/io/github/xnovo3000/eventus/api/repository/AuthorityRepository.java
+++ src/main/java/io/github/xnovo3000/eventus/api/repository/AuthorityRepository.java
@@ -3,9 +3,7 @@ package io.github.xnovo3000.eventus.api.repository;
 import io.github.xnovo3000.eventus.api.entity.Authority;
 import io.github.xnovo3000.eventus.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface AuthorityRepository extends JpaRepository<Authority, Long> {
     void deleteAllByUser(User user);
 }
--- src/main/java/io/github/xnovo3000/eventus/api/repository/SubscriptionRepository.java
+++ src/main/java/io/github/xnovo3000/eventus/api/repository/SubscriptionRepository.java
@@ -4,11 +4,9 @@ import io.github.xnovo3000.eventus.api.entity.Event;
 import io.github.xnovo3000.eventus.api.entity.Subscription;
 import io.github.xnovo3000.eventus.api.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     Optional<Subscription> findByUserAndEvent(User user, Event event);
 }
