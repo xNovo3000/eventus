cc38d3e52e1af60b85112a6808fc017f6a8db1ba
xNovo3000 <fontana.ciro.2001@gmail.com>
2023-06-11 00:12:39 +0200

    Api fix, added last package-info.java

--- api/src/main/java/io/github/xnovo3000/eventus/api/dto/input/RemoveSubscriptionDto.java
+++ api/src/main/java/io/github/xnovo3000/eventus/api/dto/input/RemoveSubscriptionDto.java
@@ -1,8 +1,12 @@
 package io.github.xnovo3000.eventus.api.dto.input;
 
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class RemoveSubscriptionDto {
+
+    @NotNull
     private String username;
+
 }
---
+++ service/src/main/java/io/github/xnovo3000/eventus/controller/error/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Management of error in order to send stylized error message to the frontend
+ */
+package io.github.xnovo3000.eventus.controller.error;
